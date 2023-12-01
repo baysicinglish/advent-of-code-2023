a13139b5c3b2c49677838309605a76c2475b1e23
baysicinglish <dbeazley.400@gmail.com>
2023-12-01 20:28:25 +0000

    Part two solution

--- advent/day_1/test_trebuchet.py
+++ advent/day_1/test_trebuchet.py
@@ -1,8 +1,37 @@
-from .trebuchet import get_calibration_values
+import pytest
+
+from .trebuchet import (
+    get_calibration_values,
+    get_numerical_value,
+    parse_calibration_value,
+    CalibrationValueNotFound
+)
 
 
 class TestGetCalibrationValues:
-    def test_gets_calibration_values_for_example(self):
+    def test_gets_calibration_values_for_part_one(self):
         values = get_calibration_values("part_one_example.txt")
 
         assert values == (12, 38, 15, 77)
+
+    def test_gets_calibration_values_for_part_two(self):
+        values = get_calibration_values("part_two_example.txt", include_words=True)
+
+        assert values == (29, 83, 13, 24, 42, 14, 76)
+
+
+class TestParseCalibrationValue:
+    def test_raises_exception_if_no_value_in_input(self):
+        line = "therearenonumbersinhere"
+
+        with pytest.raises(CalibrationValueNotFound):
+            parse_calibration_value(line)
+
+
+class TestGetNumericalValue:
+    def test_get_numerical_value(self):
+        line = "two1nine"
+
+        value = get_numerical_value(line)
+
+        assert value == "2"
--- advent/day_1/trebuchet.py
+++ advent/day_1/trebuchet.py
@@ -1,23 +1,55 @@
+from enum import Enum
+from num2words import num2words
 
-def get_calibration_values(input_file):
+
+class CalibrationValueNotFound(ValueError):
+    pass
+
+
+ONE_TO_TEN = Enum("Number", [num2words(num) for num in range(1, 10)])
+
+
+def get_calibration_values(input_file, include_words=False):
     with open(f"day_1/inputs/{input_file}", "r") as file:
-        calibration_values = [parse_calibration_value(line) for line in file]
+        calibration_values = [parse_calibration_value(line, include_words=include_words) for line in file]
     return tuple(calibration_values)
 
 
-def parse_calibration_value(line):
+def parse_calibration_value(line, include_words=False):
+    left_digit, right_digit = None, None
     left_pointer, right_pointer = 0, len(line) - 1
 
-    while not line[left_pointer].isdigit():
+    while left_pointer <= right_pointer:
+        if line[left_pointer].isdigit():
+            left_digit = line[left_pointer]
+            break
+        elif include_words and (value := get_numerical_value(line[left_pointer:])):
+            left_digit = value
+            break
         left_pointer += 1
-    left_digit = line[left_pointer]
 
-    while not line[right_pointer].isdigit():
+    while left_pointer <= right_pointer:
+        if line[right_pointer].isdigit():
+            right_digit = line[right_pointer]
+            break
+        elif include_words and (value := get_numerical_value(line[right_pointer:])):
+            right_digit = value
+            break
         right_pointer -= 1
-    right_digit = line[right_pointer]
+
+    if not (left_digit and right_digit):
+        raise CalibrationValueNotFound(f"Could not find a calibration value for line: {line}")
 
     return int(left_digit + right_digit)
 
 
+def get_numerical_value(string):
+    for value in ONE_TO_TEN:
+        if string.startswith(value.name):
+            return str(value.value)
+    return None
+
+
 if __name__ == "__main__":
-    print(sum(get_calibration_values("part_one.txt")))
+    print(sum(get_calibration_values("challenge.txt")))
+    print(sum(get_calibration_values("challenge.txt", include_words=True)))
