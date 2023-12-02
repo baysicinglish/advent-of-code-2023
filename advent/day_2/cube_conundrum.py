43e295c7178d59260e07c1577436f2f408904dbe
baysicinglish <dbeazley.400@gmail.com>
2023-12-02 13:36:37 +0000

    Solution for part one
    Every time I make a mistake I have to write an additional test :(

---
+++ advent/day_2/cube_conundrum.py
@@ -0,0 +1,42 @@
+
+def get_possible_games(results_log, available_cubes):
+    with open(f"day_2/inputs/{results_log}") as results:
+        for line in results:
+            game_number, min_cubes_needed = parse_line(line)
+            if not is_cube_count_exceeded(available_cubes, min_cubes_needed):
+                yield game_number
+
+
+def parse_line(line):
+    """
+    Expects a game in the form `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green`
+    Returns the game number as an int and a dict containing the maximum count of all colours revealed during the game
+    """
+
+    game, rounds = line.split(":")
+    _, game_number = game.split()
+    game_number = int(game_number)
+
+    min_cubes_needed = {}
+
+    for round_ in rounds.split(";"):
+        for colour in round_.split(","):
+            colour.strip()
+            count, colour = colour.split()
+            colour = colour.upper()
+            min_cubes_needed[colour] = max(min_cubes_needed.get(colour, 0), int(count))
+
+    return game_number, min_cubes_needed
+
+
+def is_cube_count_exceeded(available_cubes, min_cubes_needed):
+    for colour, max_count in min_cubes_needed.items():
+        if max_count > available_cubes.get(colour, 0):
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    cubes_in_game = {"RED": 12, "GREEN": 13, "BLUE": 14}
+    possible_games = tuple(get_possible_games("challenge.txt", cubes_in_game))
+    print(sum(possible_games))
---
+++ advent/day_2/test_cube_conundrum.py
@@ -0,0 +1,61 @@
+from .cube_conundrum import get_possible_games, parse_line, is_cube_count_exceeded
+
+
+class TestGetPossibleGames:
+    def test_returns_ids_of_possible_games(self):
+        available_colours = {
+            "RED": 12,
+            "GREEN": 13,
+            "BLUE": 14
+        }
+
+        possible_games = get_possible_games("example.txt", available_colours)
+
+        assert tuple(possible_games) == (1, 2, 5)
+
+
+class TestParseLine:
+    def test_returns_number_and_min_cubes_needed(self):
+        line = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+
+        game_number, revealed_colours = parse_line(line)
+
+        assert game_number == 1
+        assert revealed_colours == {
+            "BLUE": 6,
+            "RED": 4,
+            "GREEN": 2
+        }
+
+    def test_returns_multiple_digit_game_number(self):
+        line = "Game 100: 3 blue, 4 red"
+
+        game_number, _ = parse_line(line)
+
+        assert game_number == 100
+
+
+class TestIsCubeCountExceeded:
+    def test_cube_count_not_exceeded_returns_false(self):
+        available_cubes = {'RED': 4, 'GREEN': 13, 'BLUE': 14}
+        min_game_cubes = {'BLUE': 6, 'RED': 4, 'GREEN': 2}
+
+        result = is_cube_count_exceeded(available_cubes, min_game_cubes)
+
+        assert result is False
+
+    def test_cube_count_exceeded_returns_true(self):
+        available_cubes = {'RED': 12, 'GREEN': 13, 'BLUE': 14}
+        min_game_cubes = {'BLUE': 6, 'RED': 20, 'GREEN': 13}
+
+        result = is_cube_count_exceeded(available_cubes, min_game_cubes)
+
+        assert result is True
+
+    def test_unexpected_cube_colour_returns_true(self):
+        available_cubes = {'RED': 5}
+        min_game_cubes = {'RED': 4, 'BLACK': 1}
+
+        result = is_cube_count_exceeded(available_cubes, min_game_cubes)
+
+        assert result is True
