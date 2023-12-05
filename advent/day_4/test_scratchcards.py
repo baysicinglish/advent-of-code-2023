2175ae163cf8ca47c30393d99dce91953bfe10ec
baysicinglish <dbeazley.400@gmail.com>
2023-12-05 22:52:15 +0000

    Part two solution

--- advent/day_4/test_scratchcards.py
+++ advent/day_4/test_scratchcards.py
@@ -1,8 +1,15 @@
-from .scratchcards import calculate_scratchcard_points
+from .scratchcards import calculate_scratchcard_points, calculate_total_scratchcards
 
 
-class TestGetScratchcardPoints:
+class TestCalculateScratchcardPoints:
     def test_returns_points(self):
         points = tuple(calculate_scratchcard_points("example.txt"))
 
         assert points == (8, 2, 2, 1, 0, 0)
+
+
+class TestCalculateTotalScratchcards:
+    def test_returns_total_count(self):
+        count = calculate_total_scratchcards("example.txt")
+
+        assert count == 30
