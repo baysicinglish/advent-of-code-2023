41f15fc0fe0fc74eef64d1efa49e4bd732c16fa9
baysicinglish <dbeazley.400@gmail.com>
2023-12-09 13:15:37 +0000

    Part two solution

--- advent/day_9/mirage_maintenance.py
+++ advent/day_9/mirage_maintenance.py
@@ -1,6 +1,6 @@
 
 
-def predict_environmental_conditions(oasis_report):
+def predict_environmental_conditions(oasis_report, reverse=False):
     conditions = []
 
     with open(f"day_9/inputs/{oasis_report}", "r") as recordings:
@@ -11,22 +11,30 @@ def predict_environmental_conditions(oasis_report):
     forecast = []
 
     for condition in conditions:
-        forecast.append(predict_future_value(condition))
+        forecast.append(extrapolate_value(condition, reverse=reverse))
 
     return sum(forecast)
 
 
-def predict_future_value(history):
-    if all([value - history[-1] == 0 for value in history]):
-        return history[-1]
+def extrapolate_value(history, reverse=False):
+    index = 0 if reverse else -1
+
+    if all([value - history[index] == 0 for value in history]):
+        return history[index]
 
     changes = []
     for reading_number in range(1, len(history)):
         change = history[reading_number] - history[reading_number - 1]
         changes.append(change)
 
-    return history[-1] + predict_future_value(changes)
+    predicted_step = extrapolate_value(changes, reverse=reverse)
+
+    if reverse:
+        predicted_step *= -1
+
+    return history[index] + predicted_step
 
 
 if __name__ == "__main__":
     print(predict_environmental_conditions("challenge.txt"))
+    print(predict_environmental_conditions("challenge.txt", reverse=True))
--- advent/day_9/test_mirage_maintenance.py
+++ advent/day_9/test_mirage_maintenance.py
@@ -1,4 +1,4 @@
-from .mirage_maintenance import predict_environmental_conditions, predict_future_value
+from .mirage_maintenance import predict_environmental_conditions, extrapolate_value
 
 
 class TestPredictEnvironmentalConditions:
@@ -7,11 +7,23 @@ class TestPredictEnvironmentalConditions:
 
         assert predicted_change == 114
 
+    def test_predicts_example_total_previous_change(self):
+        predicted_change = predict_environmental_conditions("example.txt", reverse=True)
 
-class TestPredictFutureValue:
+        assert predicted_change == 2
+
+
+class TestExtrapolateValue:
     def test_predicts_condition_change(self):
         history = [10, 13, 16, 21, 30, 45]
 
-        predicted_value = predict_future_value(history)
+        predicted_value = extrapolate_value(history)
 
         assert predicted_value == 68
+
+    def test_predicts_previous_condition_change(self):
+        known_history = [10, 13, 16, 21, 30, 45]
+
+        predicted_value = extrapolate_value(known_history, reverse=True)
+
+        assert predicted_value == 5
